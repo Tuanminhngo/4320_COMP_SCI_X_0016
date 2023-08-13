e6c44c96a26373775e4b5d5241a0a0ad78bd5559
Tuanminhngo <140896234+Tuanminhngo@users.noreply.github.com>
2023-08-14 00:30:16 +0930

    ws04 update

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
-void print_matrix(int array[10][10]) {
+int *readNumbers() {
+    int *numbers = new int[10];  // Dynamically allocate an array of int
 
-  
+    for (int i = 0; i < 10; i++) {
+        std::cin >> numbers[i];  // Read integers from input
+    }
+
+    return numbers;
+}
+
+void printNumbers(int *numbers, int length) {
+    for (int i = 0; i < length; i++) {
+        std::cout << i << " " << numbers[i] << std::endl;
+    }
 }
