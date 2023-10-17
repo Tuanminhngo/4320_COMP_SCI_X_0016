ce5f7e84e506acdd0d1cd73d3337865af4da9a66
Tuanminhngo <140896234+Tuanminhngo@users.noreply.github.com>
2023-10-17 19:20:45 +1030

    ws11

--- ws11-1.cpp
+++ ws11-1.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
@@ -10,39 +10,55 @@ typedef vector<int> Records;
 
 // https://www.cplusplus.com/doc/tutorial/files/
 class RecordsManager {
-private:
-    fstream _file;
-    string _filename;
+ private:
+  fstream _file;
+  string _filename;
 
-public:
-    RecordsManager(string filename) : _filename(filename) {}
-    void read(Records &records) {
-        _file.open(_filename, ios::in);
-        if (_file.is_open()) {
-            string line;
-            while (std::getline(_file, line)) {
-                records.push_back(stoi(line));
-            }
-            _file.close();
-        }
+ public:
+  RecordsManager(string filename) : _filename(filename) {}
+  void read(Records& records) {
+    _file.open(_filename, ios::in);
+    if (_file.is_open()) {
+      string line;
+      while (std::getline(_file, line)) {
+        records.push_back(stoi(line));
+      }
+      _file.close();
     }
-};
+  }
 
-int main() {
-        
-        RecordsManager receordM("test_clean.txt");
-        // RecordsManager receordM("test_corrupt1.txt");
-        // RecordsManager receordM("test_corrupt2.txt");
-        Records myRecords;
-
-        // reads records
-        receordM.read(myRecords);
-
-        // calculate and print out the sum
-        int sum = 0;
-        for (int i = 0; i < myRecords.size(); i++) {
-             sum += myRecords[i];
+  void read(Records& records) {
+    _file.open(_filename, ios::in);
+    if (_file.is_open()) {
+      string line;
+      while (getline(_file, line)) {
+        // when stoi fails use try catch
+        try {
+          records.push_back(stoi(line));
+        } catch (exception e) {
+          cout << "Error: " << e.what() << endl;
         }
-        cout << sum << endl;
+      }
+      _file.close();
+    }
+  }
+
+  ;
+
+  int main() {
+    RecordsManager receordM("test_clean.txt");
+    // RecordsManager receordM("test_corrupt1.txt");
+    // RecordsManager receordM("test_corrupt2.txt");
+    Records myRecords;
+
+    // reads records
+    receordM.read(myRecords);
+
+    // calculate and print out the sum
+    int sum = 0;
+    for (int i = 0; i < myRecords.size(); i++) {
+      sum += myRecords[i];
+    }
+    cout << sum << endl;
     return 0;
-}
+  }
